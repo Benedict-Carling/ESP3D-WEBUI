fdfc6e01cdfe50a17336bbb2ecc0064ad1d84b02
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2022-08-25 10:01:00 +0100

    chore: Saving text is now functional

--- www/js/liblouis.js
+++ www/js/liblouis.js
@@ -6,3 +6,9 @@ function logInputText() {
   processedText.value = initialInputText.value;
   console.log("wrote to processedText");
 }
+
+function saveTextInput() {
+  console.log("Attempting to save");
+  SPIFFS_UploadText(initialInputText.value);
+  console.log("Saved Text");
+}
