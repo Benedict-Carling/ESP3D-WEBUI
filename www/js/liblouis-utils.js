84d37eca729fd3bbade0bb76e652e5beb27fd98e
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2022-09-09 10:40:12 +0100

    chore: Don't execute macro on 15th click

--- www/js/liblouis-utils.js
+++ www/js/liblouis-utils.js
@@ -4,14 +4,8 @@ var processedText = document.querySelector("#processedText");
 liblouis.enableOnDemandTableLoading();
 
 function logInputText(inputCharacter) {
-  console.log("inputCharacter", inputCharacter);
-  console.info(
-    "Liblouis Version with ccall:",
-    liblouisBuild.ccall("lou_version", "string")
-  );
-  console.info("Liblouis Version easy api:", liblouis.version());
-  console.info("Liblouis Version easy api:", liblouis.version());
-  console.log({ PAIGE_POTENT_VALUE: PAIGE_POTENT_VALUE });
+  liblouisTranslation();
+  checkPotentiometerValue();
   console.log(inputCharacter.split("\n"));
   var lines = inputCharacter.split("\n");
   var lastLineIsEmpty = lines.at(-1).length === 0;
@@ -26,18 +20,29 @@ function logInputText(inputCharacter) {
   if (lastLineIsEmpty && !secondLastLineHas15) {
     initialInputText.value = inputCharacter.trim();
     console.log("trimmed line");
-  }
-  if (lines.at(-1).length > 15) {
+  } else if (lines.at(-1).length > 15) {
     initialInputText.value = inputCharacter.slice(0, -1);
+  } else {
+    processedText.value = initialInputText.value;
+    console.log("valid input recieved; executing gcode");
+    macro_command("SD", "paige.gcode");
   }
-  processedText.value = initialInputText.value;
-  console.log("wrote to processedText");
-  console.log("About to attempt to run gcode");
-  macro_command("SD", "paige.gcode");
 }
 
 function saveTextInput() {
   console.log("Attempting to save");
   SPIFFS_UploadText(initialInputText.value);
   console.log("Saved Text");
+}
+
+function liblouisTranslation() {
+  console.info(
+    "Liblouis Version with ccall:",
+    liblouisBuild.ccall("lou_version", "string")
+  );
+  console.info("Liblouis Version easy api:", liblouis.version());
+}
+
+function checkPotentiometerValue() {
+  console.log({ PAIGE_POTENT_VALUE: PAIGE_POTENT_VALUE });
 }
